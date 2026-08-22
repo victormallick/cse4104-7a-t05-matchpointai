9dc97d28eab3170922f5a517539548bfa068889f
Wizardezgg <victormallick19@gmail.com>
2026-08-23 02:26:19 +0600

    vite fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,4 +13,21 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor-react';
+            }
+            if (id.includes('lucide-react')) {
+              return 'vendor-icons';
+            }
+          }
+        }
+      }
+    }
+  }
 });
